b45334df3bd56719b8deafbfdd8a42ddf676decb
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2017-05-11 11:58:26 -0500

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -43,7 +43,6 @@ module.exports = function(grunt) {
                             'node_modules/angular/angular.min.js',
                             'node_modules/angular-animate/angular-animate.min.js',
                             'node_modules/angular-aria/angular-aria.min.js',
-                            'node_modules/angular-messages/angular-messages.min.js',
                             'node_modules/angular-route/angular-route.min.js'
                         ],
                         dest: 'dist/assets/js/',
